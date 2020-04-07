247750510d48dc75b685c50b8a687bf97481a2b7
mdfaizan7 <faizaanazim@gmail.com>
2020-04-07 19:29:30 +0530

    added routes and navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from "react";
-import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+// css
+import "semantic-ui-css/semantic.min.css";
+
+import { Container } from "semantic-ui-react";
+// pages
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+
+// components
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World!!</h1>
-    </div>
+    <Container>
+      <Router>
+        <Navbar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/login" component={Login} />
+      </Router>
+    </Container>
   );
 }
 
---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React, { useState } from "react";
+import { Menu } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  const pathname = window.location.pathname;
+  console.log(pathname);
+  const path =
+    pathname === "/" ? "Home" : pathname === "/login" ? "Login" : "Register";
+
+  const [activeItem, setActiveItem] = useState(path);
+
+  const handleItemClick = (e, { name }) => setActiveItem(name);
+
+  return (
+    <Menu pointing secondary size="massive" color="teal">
+      <Menu.Item
+        name="Home"
+        active={activeItem === "Home"}
+        onClick={handleItemClick}
+        as={Link}
+        to="/"
+      />
+      <Menu.Menu position="right">
+        <Menu.Item
+          name="Login"
+          active={activeItem === "Login"}
+          onClick={handleItemClick}
+          as={Link}
+          to="/login"
+        />
+        <Menu.Item
+          name="Register"
+          active={activeItem === "Register"}
+          onClick={handleItemClick}
+          as={Link}
+          to="/register"
+        />
+      </Menu.Menu>
+    </Menu>
+  );
+};
+
+export default Navbar;
