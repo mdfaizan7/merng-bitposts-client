e575e06e2c0536947080d0de293c336258da29d7
mdfaizan7 <faizaanazim@gmail.com>
2020-04-11 18:34:29 +0530

    handled errors

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -13,15 +13,19 @@ const CreatePost = () => {
   const [createPost, { error }] = useMutation(CREATE_POST_MUTATION, {
     variables: values,
     update(proxy, result) {
-      const data = proxy.readQuery({
-        query: FETCH_POSTS_QUERY,
-      });
-      proxy.writeQuery({
-        query: FETCH_POSTS_QUERY,
-        variables: values,
-        data: { getPosts: [result.data.createPost, ...data.getPosts] },
-      });
-      values.body = "";
+      try {
+        const data = proxy.readQuery({
+          query: FETCH_POSTS_QUERY,
+        });
+        proxy.writeQuery({
+          query: FETCH_POSTS_QUERY,
+          variables: values,
+          data: { getPosts: [result.data.createPost, ...data.getPosts] },
+        });
+        values.body = "";
+      } catch (err) {
+        console.log(err);
+      }
     },
   });
 
@@ -35,16 +39,24 @@ const CreatePost = () => {
         <h3>Create a Post:</h3>
         <Form.Field>
           <Form.Input
-            placeholder="This is start of something bueatiful, say something nice or share a cat fact"
+            placeholder="Write the body of post..."
             name="body"
             onChange={onChange}
             value={values.body}
+            error={error ? true : false}
           />
           <Button type="submit" color="teal">
             Submit
           </Button>
         </Form.Field>
       </Form>
+      {error && (
+        <div className="ui error message" style={{ marginBottom: 20 }}>
+          <ul className="list">
+            <li>{error.graphQLErrors[0].message}</li>
+          </ul>
+        </div>
+      )}
     </React.Fragment>
   );
 };
